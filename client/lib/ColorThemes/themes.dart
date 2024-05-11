b27ee1c22f8f86198b223eeb3faacd1b50abf76d
Derek Naing <dereknaing01@gmail.com>
2024-05-11 00:12:06 -0700

    Removed Flutter base example code and added light and dark themes

---
+++ client/lib/ColorThemes/themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyAppThemes {
+
+  static final darkTheme = ThemeData(
+    primaryColor: Colors.black,
+    brightness: Brightness.dark,
+  );
+
+  static final lightTheme = ThemeData(
+    primaryColor: Colors.white,
+    brightness: Brightness.light,
+  );
+  
+}
