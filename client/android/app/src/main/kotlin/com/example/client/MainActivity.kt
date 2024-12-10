2c933f35086b197a360fe6b4382edd4965012a29
Derek Naing <dereknaing01@gmail.com>
2024-12-09 21:15:56 -0800

    Updated all dependencies

--- client/android/app/src/main/kotlin/com/example/client/MainActivity.kt
+++ client/android/app/src/main/kotlin/com/example/client/MainActivity.kt
@@ -1,36 +1,5 @@
 package com.example.client
 
-
-import android.content.Context
-import android.media.AudioManager
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
-
-    private val CHANNEL = "com.example.client/audio"
-
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "stopAudio") {
-                stopAudio()
-                result.success(null)
-            } else {
-                result.notImplemented()
-            }
-        }
-    }
-
-
-    private fun stopAudio() {
-        //Pause music
-        val am = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-
-        am.requestAudioFocus(null,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
-    }
-
-}
+class MainActivity: FlutterActivity()
